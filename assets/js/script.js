815860b08d2c5bbf7d695f4696ec935bdb287533
Antony Valete <antonyvalete@gmail.com>
2022-06-08 22:06:43 -0300

    add: Adding the first files of the project and getting the main skeleton done.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var usuario_careca = {
+    nome_usuario: "Caio Careca"
+}
+
+var usuario_cabelo = {
+    nome_usuario: "Caio Cabelo"
+}
+
+function enviarTexto(element){
+    let new_msg = document.getElementById("nova-mensagem")
+
+    
+}
+
