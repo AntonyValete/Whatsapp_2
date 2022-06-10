6869403108fd90070c34c57b468c7684014d827a
Elias <pinheirinho2004@gmail.com>
2022-06-10 10:22:00 -0300

    update: Funcionamento do campo de mensagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,22 @@ var usuario_cabelo = {
     nome_usuario: "Caio Cabelo"
 }
 
-function enviarTexto(element){
-    let new_msg = document.getElementById("nova-mensagem")
+function enviarTexto(id) {
+    let new_msg = document.getElementById("nova-mensagem").content.querySelector("div").cloneNode(true)
+    let campo_msg = document.getElementById(id).value
+    let log_msg = document.querySelector("main")
 
-    
+    if (campo_msg.value != "") {
+        new_msg.querySelector("p").innerHTML = campo_msg.value
+        campo_msg.value = ""
+
+        if (id == "texto-careca") {
+            new_msg.className += " align-direita"
+        } else {
+            new_msg.className += " align-esquerda"
+        }
+
+        log_msg.append(new_msg)
+    }
 }
 
